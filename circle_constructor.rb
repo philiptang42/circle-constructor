46bfc65f99f019a212466bf9a5a7267ff4acbdd0
philiptang42 <philiptang42@gmail.com>
2015-12-25 16:15:14 -0500

    part 2 solution

--- circle_constructor.rb
+++ circle_constructor.rb
@@ -3,8 +3,12 @@ require 'pry'
 class Circle
   attr_reader :radius
 
-  def initialize(radius)
-    @radius = radius
+  def initialize(options_or_radius)
+    if options_or_radius[:radius].nil?
+      @radius = options_or_radius[:diameter] / 2
+    else
+      @radius = options_or_radius[:radius]
+    end
   end
 
 end
